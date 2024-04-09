85fd871ec12ee5369b68e74d593bd09c950ca055
iosdevSW <iosdev.sw@gmail.com>
2024-04-09 14:12:13 +0900

    Initial Commit

---
+++ TaskGroupExample/TaskGroupExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskGroupExampleApp.swift
+//  TaskGroupExample
+//
+//  Created by iOS신상우 on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskGroupExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
